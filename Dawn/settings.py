f7cd0af3c651a4c9957f1493521eb34c638156c7
lue_liu <lue_liu@qq.com>
2014-02-17 22:07:36 +0800

    settings database

--- Dawn/settings.py
+++ Dawn/settings.py
@@ -60,7 +60,7 @@ WSGI_APPLICATION = 'Dawn.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'database', 'dawn.db'),
     }
 }
 
