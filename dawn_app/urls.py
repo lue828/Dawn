8f0e25960147c11bc37839706eb86681de3bec74
lue_liu <lue_liu@qq.com>
2014-02-20 08:07:51 +0800

    add some static

--- dawn_app/urls.py
+++ dawn_app/urls.py
@@ -4,4 +4,7 @@ urlpatterns = patterns(('dawn_app.views'),
     url(r'^bloglist/$', 'blog_list', name= 'bloglist'),
     url(r'^blog/(?P<id>\d+)/$', 'blog_show', name='detailblog'),
     url(r'^blog/tag/(?P<id>\d+)/$', 'blog_filter', name='filtrblog'),
+    url(r'^blog/add/$', 'blog_add', name='addblog'),
+    url(r'^blog/(?P<id>\w+)/update/$', 'blog_update', name='updateblog'),
+    url(r'^blog/(?P<id>\w+)/del/$', 'blog_del', name='delblog'),
 )
--- dawn_app/views.py
+++ dawn_app/views.py
@@ -7,6 +7,8 @@ from django.core.context_processors import csrf
 from dawn_app.models import Blog
 from dawn_app.models import Author
 from dawn_app.models import Tag
+from dawn_app.forms import BlogForm
+from dawn_app.forms import TagForm
 
 def blog_list(request):
     blogs = Blog.objects.all()
@@ -25,3 +27,107 @@ def blog_filter(request, id=''):
     blogs = tag.blog_set.all()
     return render_to_response("blog_filter.html",
         {"blogs": blogs, "tag": tag, "tags": tags})
+
+def blog_add(request):
+    if request.method == "POST":
+        form = BlogForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            title = cd['caption']
+            author = Author.objects.get(id=1)
+            content = cd['content']
+            blog = Blog(caption=title, author=author, content=content)
+            blog.save()
+            id = Blog.objects.order_by('-publish_time')[0].id
+            return HttpResponseRedirect('/dawn_app/blog/%s' % id)
+        else:
+            form = BlogForm()
+        return render_to_response('blog_add.html',
+            {'form': form}, context_intance=RequestContext(request))
+
+def blog_add(request):
+    if request.method == 'POST':
+        form = BlogForm(request.POST)
+        tag = TagForm(request.POST)
+        if form.is_valid() and tag.is_valid():
+            cd = form.cleaned_data
+            cdtag = form.cleaned_data
+            tagname = cdtag['tag_name']
+            for taglist in tagname.split():
+                tag.objects.get_or_create(tag_name=taglist.strip())
+            title = cd['caption']
+            author = Author.objects.get(id=1)
+            content = cd['content']
+            blog = Blog(caption=title, author=author, content=content)
+            blog.save()
+            for taglist in tagname.split():
+                blog.tags.add(Tag.objects.get(tag_name=taglist.strip()))
+                blog.save()
+            id = Blog.objects.order_by('-publish_time')[0].id
+            return HttpResponseRedirect('/dawn_app/blog/%s' % id)
+        else:
+            form = BlogForm()
+            tag = TagForm(initial={'tag_name': 'notags'})
+        return render_to_response('blog_add.html',
+            {'form': form, 'tag':tag}, context_instance=RequestContext(request))
+
+def blog_update(request, id=""):
+    id = id
+    if request.method == 'POST':
+        form = BlogForm(request.POST)
+        tag = TagForm(request.POST)
+        if form.is_valid() and tag.is_valid():
+            cd = form.cleaned_data
+            cdtag = tag.cleaned_data
+            tagname = cdtag['tag_name']
+            tagnamelist = tagname.split()
+            for taglist in tagnamelist:
+                Tag.objects.get_or_create(tag_name=taglist.strip())
+            title = cd['caption']
+            content = cd['content']
+            blog = Blog.objects.get(id=id)
+            if blog:
+                blog.caption = title
+                blog.content = content
+                blog.save()
+                for taglist in tagnamelist:
+                    blog.tags.add(Tag.objects.get(tag_name=taglist.strip()))
+                    blog.save()
+                tags = blog.tags.all()
+                for tagname in tags:
+                    tagname = unicode(str(tagname), "utf-8")
+                    if tagname not in tagnamelist:
+                        notag = blog.tags.get(tag_name=tagname)
+                        blog.tags.remove(notag)
+            else:
+                blog = Blog(caption=blog.caption, content=blog.content)
+                blog.save()
+            return HttpResponseRedirect('/dawn_app/blog/%s' % id)
+    else:
+        try:
+            blog = Blog.objects.get(id=id)
+        except Exception:
+            raise Http404
+        form = BlogForm(initial={'caption': blog.caption, 'content': blog.content}, auto_id=False)
+        tags = blog.tags.all()
+        if tags:
+            taginit = ''
+            for x in tags:
+                taginit += str(x) + ' '
+            tag = TagForm(initial={'tag_name': taginit})
+        else:
+            tag = TagForm()
+    return render_to_response('blog_add.html',
+        {'blog': blog, 'form': form, 'id': id, 'tag': tag},
+        context_instance=RequestContext(request))
+
+def blog_del(request, id=""):
+    try:
+        blog = Blog.objects.get(id=id)
+    except Exception:
+        raise Http404
+    if blog:
+        blog.delete()
+        return HttpResponseRedirect("/dawn_app/bloglist/")
+    blogs = Blog.objects.all()
+    return render_to_response("blog_list.html", {"blogs": blogs})
