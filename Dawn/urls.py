1dc12fa634c3d193f3c92e437520aabe1b7a25dc
lue_liu <lue_liu@qq.com>
2014-02-20 22:16:00 +0800

    add some apps

--- Dawn/urls.py
+++ Dawn/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
 
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'Dawn.views.home', name='home'),
